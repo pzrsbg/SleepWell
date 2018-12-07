5a9ac843746f6a98d698bacf419167ccdf4c5a68
Kamil Rojek <kamilrojek@hotmail.com>
2018-12-07 01:09:30 +0100

    Owin + Widok głównej

---
+++ SleepWell/Startup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(SleepWell.Startup))]
+
+namespace SleepWell
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
