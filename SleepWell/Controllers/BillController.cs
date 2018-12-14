c33f818ced151b5b5f7229835b5ab9eb58502a96
Rafał Borycki <rafalborycki@outlook.com>
2018-12-14 09:53:00 +0100

    Bill test

---
+++ SleepWell/Controllers/BillController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+
+namespace SleepWell.Controllers
+{
+
+    public class BillController : Controller
+    {
+        public Class1()
+        {
+
+
+        }
+    }
+
+
+}
